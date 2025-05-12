20b69af868b752ca5cc9bd77fd84539321108957
Shawn_Pantz <2001shawnp@gmail.com>
2025-05-12 17:48:48 +0500

    cleanup hero and checkpoint

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -4,39 +4,34 @@ import {useTranslation} from "react-i18next";
 import TypeWriter from "./typeWriter/typeWriter.tsx";
 
 const Hero = ({scrollToSection, projectsRef, contactRef}: HeroProps) => {
-
     const {t} = useTranslation();
 
     return (
-        <section id="hero-section">
-            <div className="hero-content">
-
+        <section id="hero-section" className="d-flex align-items-center position-relative overflow-hidden">
+            <div className="hero-content position-relative">
                 <div className="typewriter-container">
                     <TypeWriter/>
                 </div>
 
-                {/*<div>{t('hero.test')}</div>*/}
-
-                <p className="description">
+                <p className="description text-white">
                     {t('hero.description1')}
                     <br/><br/>
                     {t('hero.description2')}
-                    {/*<br/><br/>*/}
-                    {/*{t('hero.description3')}*/}
                     <br/><br/>
                     {t('hero.description4')}
                 </p>
 
-                <div className="cta-buttons">
+                <div className="d-flex gap-3 mt-4 ps-1 pb-1" style={{width: '99%'}}>
+
                     <button
-                        className="btn primary"
+                        className="btn-hero btn-primary"
                         onClick={() => scrollToSection(projectsRef)}
                     >
                         {t('hero.viewWork')}
                     </button>
 
                     <button
-                        className="btn secondary"
+                        className="btn-hero btn-secondary"
                         onClick={() => scrollToSection(contactRef)}
                     >
                         {t('hero.getInTouch')}
